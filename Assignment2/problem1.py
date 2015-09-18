992a6065266b11e500fb21f6db0f65c1171ca111
ceebs03 <nick.cebry@gmail.com>
2015-09-17 20:00:01 -0400

    Started to implement the reproduce function for problem 1

--- Assignment2/problem1.py
+++ Assignment2/problem1.py
@@ -13,6 +13,21 @@ class AddingGA(BaseGA):
 		pass
 
 	def reproduce(self, parent_x, parent_y):
+		# generate a split index
+		split = random.randint(1, len(parent_x) - 1)
+		
+		# generate the sub-lists from the split
+		x_left = list(parent_x[0:split-1])
+		x_right = list(parent_x[split:])
+		y_left = list(parent_y[0:split-1])
+		y_right = list(parent_y[split:])
+		
+		# merge the sublists to create children
+		child_a = x_left + y_right
+		child_b = y_left + x_right
+		
+		# return something...
+		
 		pass
 
 	def mutate(self, child):

51bca0be36db47532fac02f716167c22af66fc6c
Morgan Hopeman <mwhopeman@wpi.edu>
2015-09-22 19:22:52 -0400

    Bin check and generate population
    Made a check make sure all bins only have 10 numbers
    
    Defined generatePopulation

--- Assignment2/problem2.py
+++ Assignment2/problem2.py
@@ -1,4 +1,5 @@
 from ga_abstract import GeneticAlgorithm
+import random
 
 # Artificial Intelligence Assignment 2 Problem 2
 
@@ -8,8 +9,54 @@ class BinGA(GeneticAlgorithm):
         # initialize population
         pass
 
+    def binCheck(self, child):
+        bc_one = 0
+        bc_two = 0
+        bc_three = 0
+        for i in range(len(child)):
+            if child[i] == 1:
+                if bc_one < 10:
+                    bc_one += 1
+                else:
+                    if bc_two < 10:
+                        child[i] = 2
+                        bc_two += 1
+                    else:
+                        child[i] = 3
+                        bc_three += 1
+            elif child[i] == 2:
+                if bc_two < 10:
+                    bc_two += 1
+                else:
+                    if bc_one < 10:
+                        child[i] = 1
+                        bc_one += 1
+                    else:
+                        child[i] = 3
+                        bc_three += 1
+            else:
+                if bc_three < 10:
+                    bc_three += 1
+                else:
+                    if bc_one < 10:
+                        child[i] = 1
+                        bc_one += 1
+                    else:
+                        child[i] = 2
+                        bc_two += 1
+
+
+
     def generatePopulation(self):
-        pass
+        POP_SIZE = 20
+        population = []
+        for i in range(POP_SIZE):
+            individual = []
+            for j in range(len(self.traits)):
+                individual.append(random.randint(1,2,3))
+            population.append(individual)
+            self.binCheck(self, population)
+        return population
 
     def fitnessFn(self, child):
         b_one = 1
@@ -29,3 +76,6 @@ class BinGA(GeneticAlgorithm):
 
     def mutate(self, child):
         pass
+
+    def str_phenotype(self, child):
+        return self.filter_traits(child)

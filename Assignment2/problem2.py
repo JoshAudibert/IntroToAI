9869b4d53607f32639698efd901186e0a2899a54
Morgan Hopeman <mwhopeman@wpi.edu>
2015-09-22 18:47:38 -0400

    Fitness Function 2
    Wrote the 2nd fitness function

--- Assignment2/problem2.py
+++ Assignment2/problem2.py
@@ -3,21 +3,29 @@ from ga_abstract import GeneticAlgorithm
 # Artificial Intelligence Assignment 2 Problem 2
 
 class BinGA(GeneticAlgorithm):
-	def __init__(self):
-		# initialize population
-		pass
-		
-	def generatePopulation(self):
-		pass
+    def __init__(self, traits):
+        self.traits = list(traits)
+        # initialize population
+        pass
 
-	def fitnessFn(self, child):
-		pass
+    def generatePopulation(self):
+        pass
 
-	def randomSelection(self, population, fitnessFn):
-		pass
+    def fitnessFn(self, child):
+        b_one = 1
+        b_two = 0
+        for i in range(len(child)):
+            if child[i] == 1:
+                b_one *= self.traits[i]
+            elif child[i] == 2:
+                b_two += self.traits[i]
+        return sum(b_one, b_two)
 
-	def reproduce(self, parent_x, parent_y):
-		pass
+    def randomSelection(self, population, fitnessFn):
+        pass
 
-	def mutate(self, child):
-		pass
+    def reproduce(self, parent_x, parent_y):
+        pass
+
+    def mutate(self, child):
+        pass

dd959b8cb659ed35f7a3d1de694b37f71a6f0186
ceebs03 <nick.cebry@gmail.com>
2015-09-22 21:10:24 -0400

    Implemented generate population function for problem 3

--- Assignment2/problem3.py
+++ Assignment2/problem3.py
@@ -11,22 +11,36 @@ class towerPiece:
         self.cost = selHeuristic 
 
 class TowerGA(GeneticAlgorithm):
-	def __init__(self, pieces):
-		# initialize population
-		self.pieces = list(pieces)
+    def __init__(self, pieces):
+        # initialize population
+        self.pieces = list(pieces)
 
-	def generatePopulation(self):
-		pass
-	
-	def fitnessFn(self, child):
-		pass
+    def generatePopulation(self):
+        POP_SIZE = 20
+        population = []
+        while(len(population) < POP_SIZE):
+            tower_size = random.randint(1,len(self.pieces))
+            tower = [0 for x in range(tower_size)]
+            pieces_left = [1 for x in range(len(self.pieces))]
+            for i in range(tower_size):
+                next_piece = 0
+                while next_piece == 0:
+                    next_index = random.randint(0,len(self.pieces)-1)
+                    next_piece = pieces_left[next_index]
+                    pieces_left[next_index] = 0
+                tower[i] = next_index
+            population.append(tower)
+        return population
+    
+    def fitnessFn(self, child):
+        pass
 
-	def randomSelection(self, population, fitnessFn):
-		pass
+    def randomSelection(self, population, fitnessFn):
+        pass
 
-	def reproduce(self, parent_x, parent_y):
-		pass
+    def reproduce(self, parent_x, parent_y):
+        pass
 
-	def mutate(self, child):
-		pass
+    def mutate(self, child):
+        pass
 

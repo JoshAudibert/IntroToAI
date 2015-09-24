69aa5b55f96667e860c219c6c1d9dc0989ba3189
bshappell <bshappell@wpi.edu>
2015-09-24 15:10:17 -0400

    updated print outs

--- Assignment2/runTest.py
+++ Assignment2/runTest.py
@@ -3,22 +3,31 @@ import ga
 
 # parse the command line inputs, run A*, print the results
 def main():
+    print "Problem 1 Test 1:"
     ga1 = ga.parseInput(1, 'problem1_test1.txt')
     ga.runGA(ga1, 10)
+    print "Problem 1 Test 2:"
     ga1 = ga.parseInput(1, 'problem1_test2.txt',)
     ga.runGA(ga1, 10)
+    print "Problem 1 Test 3:"
     ga1 = ga.parseInput(1, 'problem1_test3.txt')
     ga.runGA(ga1, 10)
+    print "Problem 2 Test 1:"
     ga1 = ga.parseInput(2, 'problem2_test1.txt')
     ga.runGA(ga1, 10)
+    print "Problem 2 Test 2:"
     ga1 = ga.parseInput(2, 'problem2_test2.txt')
     ga.runGA(ga1, 10)
+    print "Problem 2 Test 3:"
     ga1 = ga.parseInput(2, 'problem2_test3.txt')
     ga.runGA(ga1, 10)
+    print "Problem 3 Test 1:"
     ga1 = ga.parseInput(3, 'problem3_test1.txt')
     ga.runGA(ga1, 10)
+    print "Problem 3 Test 2:"
     ga1 = ga.parseInput(3, 'problem3_test2.txt')
     ga.runGA(ga1, 10)
+    print "Problem 3 Test 3:"
     ga1 = ga.parseInput(3, 'problem3_test3.txt')
     ga.runGA(ga1, 10)
     

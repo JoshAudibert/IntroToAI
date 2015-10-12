c5c90bc5606c13f3c2718325273977bba1a810d9
bshappell <bshappell@wpi.edu>
2015-10-11 22:32:46 -0400

    modified run test

--- Final_Project/runTest.py
+++ Final_Project/runTest.py
@@ -2,10 +2,25 @@ import sys
 import minesweeper
 
 def main():
-    numBombs = 10
-    numBatteries = 10
-    puzzleWidth = 10
-    puzzleHeight = 10
+    easy = True
+    medium = False
+    hard = False
+
+    if easy:
+        numBombs = 10
+        numBatteries = 10
+        puzzleWidth = 9
+        puzzleHeight = 9
+    if medium:
+        numBombs = 40
+        numBatteries = 10
+        puzzleWidth = 16
+        puzzleHeight = 16
+    if hard:
+        numBombs = 99
+        numBatteries = 10
+        puzzleWidth = 30
+        puzzleHeight = 16
 
     for i in range(10):
         print "Test ", i
